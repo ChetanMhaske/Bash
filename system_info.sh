e2cebe20fd79aaaf28b3e9ae18f390d3fae17e4c
Chetan <167629958+ChetanMhaske@users.noreply.github.com>
2025-07-24 16:07:46 +0000

    Add files via upload
    OS Name:- cat /etc/os-release
    Kernel:- uname -r
    
    CPU Info:- /proc/cpuinfo
    Cores:- nproc
    
    Memory Info:- free -h
    
    Disk Usage:- df -h /
    
    IP Address:-
    ip -4 addr show shows all IPv4 addresses.
    
    grep inet filters out only the relevant IP lines.
    
    grep -v 127.0.0.1 removes the loopback address.
    
    awk '{print $2}' grabs the IP address with CIDR (e.g. 192.168.1.5/24)
    
    cut -d/ -f1 removes the /24 part, leaving only the IP address.
    
    head -n1 grabs the first one, in case there are multiple.

---
+++ system_info.sh
@@ -0,0 +1,34 @@
+#bash
+
+echo "SYSTEM INFORMATION"
+
+#Operating System
+
+os_name=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | tr -d '"')
+
+
+#Kernel
+kernel_version=$(uname -r)
+
+echo "OS         : $os_name"
+echo "Kernel     : $kernel_version"
+echo "Uptime     : $(uptime -p)"
+
+# CPU Info
+cpu_model=$(grep -m1 'model name' /proc/cpuinfo | cut -d ':' -f2 | sed 's/^ //')
+cpu_cores=$(nproc)
+
+echo "CPU        : $cpu_model"
+echo "CPU Cores  : $cpu_cores"
+
+# Memory Info
+mem_info=$(free -h )
+echo "Memory     : $mem_info"
+
+# Disk Usage (root only)
+disk_info=$(df -h / )
+echo "Disk (/): $disk_info"
+
+# IP Address (non-loopback)
+ip_address=$(ip -4 addr show | grep inet | grep -v 127.0.0.1 | awk '{print $2}' | cut -d/ -f1 | head -n1)
+echo "IP Address : ${ip_address:-Unavailable}"
