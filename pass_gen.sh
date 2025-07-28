82e33d01f98b041b9c717d78816ffb066199a4aa
Chetan <167629958+ChetanMhaske@users.noreply.github.com>
2025-07-28 21:34:31 +0530

    Update pass_gen.sh

--- pass_gen.sh
+++ pass_gen.sh
@@ -12,9 +12,9 @@ done
 
 
 <<comment
-echo "Enter Length...":-	Prompts the user to input the desired password length
-read pass_len:-	Stores the user’s input into the variable pass_len
-for p in $(seq 1 5); do ...:-	Loops 5 times to generate 5 different passwords
-openssl rand -base64 48:-	Generates 48 random bytes and encodes them in Base64 (for high entropy)
-cut -c1-$pass_len:-	Trims the Base64 string to the user-defined length
->>
+Command/Flag		 Meaning
+read pass_len		 Takes user input for password length
+seq 1 5	  		 Generates a sequence from 1 to 5 to run the loop 5 times
+openssl rand -base64 48	 Generates a secure random 48-byte string encoded in Base64
+cut -c1-$pass_len	 Trims the password to the user-defined length
+comment
