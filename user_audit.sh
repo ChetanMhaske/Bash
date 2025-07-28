76df69fb01e15ebc55ada546e3bbf655224b51bb
Chetan <167629958+ChetanMhaske@users.noreply.github.com>
2025-07-28 21:48:45 +0530

    Update user_audit.sh

--- user_audit.sh
+++ user_audit.sh
@@ -17,10 +17,10 @@ echo "Sudo/Root users:"
 getent group sudo || getent group wheel
 
 <<comment
-Command/Section	            Meaning
-cut -d: -f1 /etc/passwd	    Lists all user accounts on the system by extracting usernames
+Command/Section	                  Meaning
+cut -d: -f1 /etc/passwd	          Lists all user accounts on the system by extracting usernames
 lastlog2	                  Displays the last login of all users (used in modern Kali, replaces lastlog)
 grep -v "Never logged in"	  Filters out users who have never logged in
-last	                      Shows recent login sessions including terminal, IP, and duration
+last	                          Shows recent login sessions including terminal, IP, and duration
 getent group sudo	          Checks if sudo group exists and lists its members
 comment
